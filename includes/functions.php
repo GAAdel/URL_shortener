c7f9b311071bc2a7e4ee681b8525e730dca0ce12
GAAdel <32236445+GAAdel@users.noreply.github.com>
2021-10-28 12:46:56 +0300

    refactoring
    creating functions with queries

--- includes/functions.php
+++ includes/functions.php
@@ -18,18 +18,41 @@ function db()
   }
 }
 
-function db_query($sql = '')
+function db_query($sql = '', $exec = false)
 {
   if (empty($sql)) return false;
-
-  $statement = db()->query($sql);
-  return $statement;
+  if ($exec) {
+    $statement = db()->exec($sql);
+    return $statement;
+  } else {
+    $statement = db()->query($sql);
+    return $statement;
+  }
 }
 
-function db_exec($sql = '')
+function get_user_count()
 {
-  if (empty($sql)) return false;
+  return db_query("SELECT COUNT(id) FROM `users`;")->fetchColumn();
+}
 
-  $statement = db()->exec($sql);
-  return $statement;
+function get_links_count()
+{
+  return db_query("SELECT COUNT(id) FROM `links`;")->fetchColumn();
+}
+
+function get_views_count()
+{
+  return db_query("SELECT SUM(`views`) FROM `links`;")->fetchColumn();
+}
+
+function get_link_info($url)
+{
+  if (empty($url)) return [];
+  return db_query("SELECT * FROM `links` WHERE `short_link` = '$url';")->fetch();
+}
+
+function update_views($url)
+{
+  if (empty($url)) return false;
+  return db_query("UPDATE `links` SET `views` = `views` + 1 WHERE `short_link` = '$url';", true);
 }
